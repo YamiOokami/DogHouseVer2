bb267864c6b31dd03671c279351bc7df49bb2526
Sela Weinrob <selaw@consist-sys.consist.co.il>
2020-12-30 17:33:05 +0200

    added active status to the nav bar

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const _ = require("lodash");
 
 const app = express();
 
+const nonActivePage = "";
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
@@ -14,15 +16,27 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("main");
+  res.render("main", {
+    aboutactive: nonActivePage,
+    contactactive: nonActivePage,
+    mainactive: "active"
+  });
 });
 
 app.get("/contact", function(req, res) {
-  res.render("contact");
+  res.render("contact", {
+    aboutactive: nonActivePage,
+    contactactive: "active",
+    mainactive: nonActivePage
+  });
 });
 
 app.get("/about", function(req, res) {
-  res.render("about");
+  res.render("about", {
+    aboutactive: "active",
+    contactactive: nonActivePage,
+    mainactive: nonActivePage
+  });
 });
 
 app.listen(3000, function() {
