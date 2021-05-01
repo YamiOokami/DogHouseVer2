f2a98714698bae3c3ee7e4ee4ca84c167ab11221
Sela Weinrob <selaweinrob@gmail.com>
2021-05-01 15:32:56 +0300

    added secure tag to nodemailer

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ let transport = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
   port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
